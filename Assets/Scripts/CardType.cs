40e2e7689a03b62cec8d464701b33f82b72f09bc
Shadowgamer567 <159549911+Shadowgamer567@users.noreply.github.com>
2026-03-24 19:14:30 +0800

    Added Basic Cards, Added Randomness to Card Selection, Made Card Creation

---
+++ Assets/Scripts/CardType.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCard", menuName = "Card")]
+public class CardType : ScriptableObject
+{
+    public string cardName;
+    public string cardDesc;
+    public int damage;
+    public int cost;
+}
